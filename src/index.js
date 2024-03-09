40bcba3ead072cc18fa84ed9b57922a6d5703a35
Keira-Bee <keira.a.burlinson@gmail.com>
2024-03-09 15:04:11 +0800

    Improved the UX when generating the poem

--- src/index.js
+++ src/index.js
@@ -16,8 +16,12 @@ function generatePoem(event) {
   let apiKey = "b2d392316cfa1a65t53d71a032b444co";
   let prompt = `User prompt: Write a poem about ${userPrompt.value}`;
   let context =
-    "You are a poet who specialises in short poetry. Your task is to write a four line poem in basic HTML with each line separated with a <br />. Sign at the bottom of the poem with `- SheCodes AI` inside a <strong> element. Please follow the user prompt.";
+    "You are a poet who specialises in short poetry. Your task is to write a four line poem in basic HTML with each line separated with a <br /> and written in black without using the <em> element or <stong> element. Do not give the poem a title. Please sign the bottom of the poem with `- SheCodes AI` inside a <strong> element. Please follow the user prompt.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `⌛ Our poet is diligently working on a poem for you about ${userPrompt.value} ⌛`;
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
